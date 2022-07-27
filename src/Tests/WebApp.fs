039788f84269beacbdba0765fdb80f184152ecb2
Sean Donaghy <s.donaghy@codat.io>
2022-07-27 06:23:13 +0100

    Remove duplicate tset code

--- src/Tests/WebApp.fs
+++ src/Tests/WebApp.fs
@@ -17,6 +17,11 @@ let dummyClient = new WebSiteManagementClient (Uri "http://management.azure.com"
 let getResourceAtIndex o = o |> getResourceAtIndex dummyClient.SerializationSettings
 
 let getResources (v:IBuilder) = v.BuildResources Location.WestEurope
+let getSlots(site:WebAppConfig) =
+    site
+    |> getResources
+    |> getResource<Arm.Web.Site>
+    |> List.filter (fun x -> x.ResourceType = Arm.Web.slots)
 
 let tests = testList "Web App Tests" [
     test "Basic Web App has service plan and AI dependencies set" {
@@ -332,11 +337,7 @@ let tests = testList "Web App Tests" [
         let site:WebAppConfig = webApp { name "slots"; add_slot slot; zip_deploy "test.zip" }
         Expect.isTrue (site.CommonWebConfig.Slots.ContainsKey "warm-up") "Config should contain slot"
 
-        let slots =
-            site
-            |> getResources
-            |> getResource<Arm.Web.Site>
-            |> List.filter (fun x -> x.ResourceType = Arm.Web.slots)
+        let slots = getSlots site 
         // Default "production" slot is not included as it is created automatically in Azure
         Expect.hasLength slots 1 "Should only be 1 slot"
     }
@@ -346,11 +347,7 @@ let tests = testList "Web App Tests" [
         let site:WebAppConfig = webApp { name "appname-that-is--exactly--40--characters"; add_slot slot; zip_deploy "test.zip" }
         Expect.isTrue (site.CommonWebConfig.Slots.ContainsKey "warm-up") "Config should contain slot"
 
-        let slots =
-            site
-            |> getResources
-            |> getResource<Arm.Web.Site>
-            |> List.filter (fun x -> x.ResourceType = Arm.Web.slots)
+        let slots = getSlots site
         // Default "production" slot is not included as it is created automatically in Azure
         Expect.hasLength slots 1 "Should only be 1 slot"
     }
@@ -360,14 +357,8 @@ let tests = testList "Web App Tests" [
         let site:WebAppConfig = webApp { name "app-name-that-is-longer-than-40-characters"; add_slot slot; zip_deploy "test.zip" }
         Expect.isTrue (site.CommonWebConfig.Slots.ContainsKey "warm-up") "Config should contain slot"
 
-        let createSlots() =
-            site
-            |> getResources
-            |> getResource<Arm.Web.Site>
-            |> List.filter (fun x -> x.ResourceType = Arm.Web.slots)
-
         let toExceptionMessage(ex:Exception) = ex.Message
-        let exceptionMessage = Expect.throwsC (fun () -> createSlots() |> ignore) toExceptionMessage
+        let exceptionMessage = Expect.throwsC (fun () -> getSlots site |> ignore) toExceptionMessage
         let expectedExceptionMessage = 
           "App name 'app-name-that-is-longer-than-40-characters' has length=42, which exceeds max length (40) so cannot be used with slots"
 
@@ -1066,3 +1057,4 @@ let tests = testList "Web App Tests" [
         Expect.hasLength site.PostDeployActions 1 "no custom post deploy actions found"
     }
 ]
+
